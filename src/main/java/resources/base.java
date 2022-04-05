db7efcbac39504fe30847bb4b619bb5e3a2a2db3
Ashish Verma <ashish.verma3@globallogic.com>
2022-04-05 18:52:53 +0530

    removed unused import

--- src/main/java/resources/base.java
+++ src/main/java/resources/base.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class base 
